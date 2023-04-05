94ae6e6d3b5d8caad1ae395467eb4c74bd58adba
Patrick M <patrickmatias.silva@hotmail.com>
2023-04-05 15:30:43 +0000

    feat: add brand logo

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -22,6 +22,7 @@ const showingNavigationDropdown = ref(false);
                             <div class="shrink-0 flex items-center">
                                 <Link :href="route('dashboard')">
                                     <ApplicationLogo
+                                        iconOnly
                                         class="block h-9 w-auto fill-current text-gray-800"
                                     />
                                 </Link>
