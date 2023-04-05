f6e8d46c31d3430ba1cead2f3c5046f128732252
Patrick M <patrickmatias.silva@hotmail.com>
2023-04-05 14:02:44 +0000

    feat: create ProductController for api resource

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all();
+
+        return response()->json([
+            'status' => true,
+            'data' => $products
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $product = Product::create($request->all());
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Product created successfully!',
+            'data' => $product
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $product = Product::findOrFail($id);
+
+        return response()->json([
+            'status' => true,
+            'data' => $product
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $product->update($request->all());
+        
+        return response()->json([
+            'status' => true,
+            'message' => 'Product updated successfully!',
+            'data' => $product
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $product->delete();
+
+        return response(null, 204);
+    }
+}
