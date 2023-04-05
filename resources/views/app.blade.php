7e685b0793fbe547c91c23e0606f2f2b609a1771
Patrick M <patrickmatias.silva@hotmail.com>
2023-04-05 15:30:58 +0000

    feat: add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,9 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
+        <link rel="icon" href="favicon.png" type="image/svg+xml" id="favicon">
+        
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
